a31b1eedfdfdf9412d57a94208750a5ab5fadd9b
Andrea <lugiolugini@gmail.com>
2021-03-02 13:34:45 +0100

    Modelli

--- Js/Libraries/utils.js
+++ Js/Libraries/utils.js
@@ -1,6 +1,22 @@
-import * as THREE from 'https://unpkg.com/three/build/three.module.js';
-export {createTexture}
+export {createTexture, loadModel}
 
 function createTexture (imgPath) { 
     return new THREE.TextureLoader().load(imgPath);
 }
+
+function loadModel(ModelName, SCENE,CAMERA) {
+    const loader = new THREE.GLTFLoader();
+    var loadedModel = null;
+    ModelName = "../../Resources/Models/asset_pack/modello.gltf";
+    loader.load(ModelName, function (gltf) { 
+        loadedModel = gltf.scene.children[0];
+        loadedModel.position.set(10,-200,-10);
+        //loadedModel.scale(1,0.1,0.1);
+        console.log(loadedModel.position)
+        console.log(typeof(loadedModel));
+        SCENE.add(loadedModel); 
+    });
+
+    return loadedModel;
+}
+
--- Js/game.js
+++ Js/game.js
@@ -1,16 +1,20 @@
-import * as THREE from 'https://unpkg.com/three/build/three.module.js';
-import {createTexture} from './Libraries/utils.js'
+import {createTexture, loadModel} from './Libraries/utils.js'
 export {CAMERA,RENDERER}
+import { OrbitControls } from 'https://unpkg.com/three@0.118/examples/jsm/controls/OrbitControls.js';
 
 //Constanti utili ---------------------------
 const SCENE = new THREE.Scene();
-const CAMERA = new THREE.PerspectiveCamera(110, window.innerWidth / window.innerHeight, 1 ,5000); //Fov-ratio-near-far
+const CAMERA = new THREE.PerspectiveCamera(110, window.innerWidth / window.innerHeight, 0.1 ,5000); //Fov-ratio-near-far
 const RENDERER = new THREE.WebGLRenderer();
 const RAYCASTER = new THREE.Raycaster();
 const MOUSE = new THREE.Vector2();
 const BASETEXTURE = createTexture("./Resources/Textures/baseTexture.png");
+const CONTROLS = new OrbitControls(CAMERA, RENDERER.domElement);
+const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+SCENE.add( light );
 //------------------------------------------
 
+loadModel('chicken', SCENE, CAMERA);
 
 
 
@@ -20,12 +24,18 @@ document.body.appendChild(RENDERER.domElement); //Inseriamo il canvas dove il re
 function animations() {}; //Da definire
 function renderLoop() { //Loop che reinderizza le immagini
     requestAnimationFrame(renderLoop);
+    CONTROLS.update();
     RENDERER.render(SCENE,CAMERA);
     animations();
 }
 renderLoop();
+window.addEventListener('resize', function () {
+    CAMERA.aspect = window.innerWidth / window.innerHeight;
+    CAMERA.updateProjectionMatrix();
+    RENDERER.setSize( window.innerWidth, window.innerHeight );
+});
 //RENDERER.setClearColor("#dfe8e8");
 SCENE.background = createTexture('../Resources/Textures/sky.jpg');
-//--- Background si setta così ----> SCENE.background = <la roba>
+CAMERA.position.set( 0, 0, 100 );//--- Background si setta così ----> SCENE.background = <la roba>
 //    O se vogliamo un colore -----> RENDERER.setClearColor("codice colore")
 //-------------------------------------------
